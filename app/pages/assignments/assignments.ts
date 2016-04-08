15bdf668e54b5e0af9370a90160af698836d8673
Tyler Renelle <tylerrenelle@gmail.com>
2016-04-08 12:07:54 -0400

    WIP redistribution fixes, auto/!auto

--- app/pages/assignments/assignments.ts
+++ app/pages/assignments/assignments.ts
@@ -2,6 +2,7 @@ import {Page} from 'ionic-angular';
 import {Patient, patients} from '../../patients';
 import * as _ from 'lodash';
 import {Dragula, DragulaService} from 'ng2-dragula/ng2-dragula';
+import {CORE_DIRECTIVES} from 'angular2/common';
 
 class Nurse {
   patients: Patient[];
@@ -13,29 +14,36 @@ class Nurse {
 
 @Page({
   templateUrl: 'build/pages/assignments/assignments.html',
-  directives: [Dragula],
+  directives: [Dragula, CORE_DIRECTIVES],
   viewProviders: [DragulaService],
 })
 export class AssignmentsPage {
   adding: boolean;
   nurses: Nurse[];
+  auto: boolean;
 
   constructor(private dragulaService: DragulaService) {
     dragulaService.setOptions('bag-one', {
       moves: (el, container, handle) => ~handle.className.indexOf('ar-move-icon')
     });
     this.nurses = JSON.parse(localStorage.getItem('nurses') || "[]");
+    this.auto = JSON.parse(localStorage.getItem('auto') || "true");
     this.redistribute();
     patients.ee.subscribe(data => this.redistribute());
   }
 
   redistribute() {
-    let {nurses} = this;
-    if (!nurses[0]) return;
+    let {nurses, auto} = this;
+    if (!(auto && nurses[0])) return;
     nurses.forEach(n => n.patients = []);
-    _.sortBy(patients.patients, 'score').reverse().forEach((p, i) => {
-      let idx = i % nurses.length;
-      nurses[idx].patients.push(p);
+
+    // Sort patients by a combo of acuity & room-to-room-distance. b-a = DESC
+    let sorted = patients.patients.sort((a, b) =>
+      b.score - a.score + Math.abs(+b.room - +a.room)
+    );
+    _.chunk(sorted, nurses.length).forEach(chunk => {
+      nurses.reverse();
+      chunk.forEach((p,i) => nurses[i].patients.push(p));
     });
   }
 
@@ -56,6 +64,14 @@ export class AssignmentsPage {
     this.nurses.splice(i,1);
     this.redistribute();
     localStorage.setItem('nurses', JSON.stringify(this.nurses));
+  }
+
+  toggleAuto() {
+    this.auto = !this.auto;
+    localStorage.setItem('auto', JSON.stringify(this.auto));
+    if (this.auto)
+      return this.redistribute();
+    // else what?
   }
 
 }
